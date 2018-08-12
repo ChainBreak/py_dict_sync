c0692955d185f541ea7dea41e8088bcdf43533d9
Thomas Rowntree <thomas.james.rowntree@gmail.com>
2018-08-12 21:57:09 +0930

    Now decodes json

--- dict_sync_server.py
+++ dict_sync_server.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python2
 
-# import socket programming library
 import socket
-
-# import thread module
 import thread
 import threading
 import time
+import json
 
 
 class DictSyncServer():
@@ -15,28 +13,42 @@ class DictSyncServer():
         sock.bind((host, port))
         sock.listen(5)
 
+        self.token_dict = {"abc123":{}}
+
         while True:
             client, addr = sock.accept()
             thread.start_new_thread(self.process_client, (client,))
         sock.close()
 
     def process_client(self,client):
+        #record process start time for detecting time out
         start_time = time.time()
+        #create empty message to append to
         msg_str = ""
+        #while connected
         while True:
-            data = client.recv(1024)
-            
-            msg_str += data
-            # print "data"
-            # print(data)
+            #read string from client and append to message string
+            msg_str += client.recv(1024)
+            #look for special character that separates json strings
+            i = msg_str.find(chr(255))
+            #if the special character is found
+            if i > 0:
+                #slice out the first json string from the message string
+                json_str = msg_str[:i]
+                #cut the json string from the front of the message string
+                msg_str = msg_str[i+1:]
+                #try passing the json string
+                try:
+                    json_dict = json.loads(json_str)
+                    print(json_dict["token"])
+                    print(len(json_dict["dict"]))
+                except Exception as e:
+                    print(e)
+
             time.sleep(0.01)
             if time.time() - start_time > 1.0:
                 break
-            # send back reversed string to client
-            #client.send(data)
 
-        print(data)
-        # connection closed
         client.close()
 
 
