ae86a236747b597a75795592bb184cadddc3be8d
Thomas Rowntree <thomas.james.rowntree@gmail.com>
2018-08-14 08:14:35 +0930

    Create classes to hold user dicts and tidy up ethernet processing

--- dict_sync_server.py
+++ dict_sync_server.py
@@ -6,6 +6,49 @@ import threading
 import time
 import json
 
+class User():
+    def __init__(self):
+        self.named_dicts = {}
+        self.lock = threading.Lock()
+
+    def __getitem__(self,key):
+        with self.lock as l:
+            if not key in self.dict:
+                self.dict[key] = NamedDict()
+            return self.dict[key]
+
+    def __delitem__(self,key):
+        pass
+
+    def __len__(self):
+        with self.lock as l:
+            return = len(self._dict)
+
+class NamedDict():
+    def __init__(self):
+        self.trans_count = 0
+        self.dict = {}
+        self.dict_trans_count = {}
+        self.dict_lock = threading.Lock()
+
+    def __getitem__(self,key):
+        with self.dict_lock as dl:
+            return self.dict.get(key,None)
+
+    def __setitem__(self,key,value):
+        with self.dict_lock as dl:
+            self.trans_count += 1
+            self.dict[key] = value
+            self.dict_trans_count = self.trans_count
+
+    def __delitem__(self,key):
+        pass
+
+    def __len__(self):
+        with self.dict_lock as dl:
+            return = len(self._dict)
+
+
 
 class DictSyncServer():
     def __init__(self,host,port):
@@ -13,37 +56,47 @@ class DictSyncServer():
         sock.bind((host, port))
         sock.listen(5)
 
-        self.token_dict = {"abc123":{}}
+        self.user_dict = {"abc123":User()}
 
         while True:
             client, addr = sock.accept()
             thread.start_new_thread(self.process_client, (client,))
         sock.close()
 
+    def process_dict(self,recv_dict):
+        print(recv_dict["token"])
+        token = recv_dict["token"]
+        name = recv_dict["name"]
+        if token in self.user_dict:
+            user = self.user_dict[token]
+            named_dict = user[name]
+
+        print(recv_dict["dict"])
+        send_dict = {}
+        return send_dict
+
     def process_client(self,client):
         #record process start time for detecting time out
         start_time = time.time()
         #create empty message to append to
-        msg_str = ""
+        recv_str = ""
         #while connected
         while True:
             #read string from client and append to message string
-            msg_str += client.recv(1024)
-            #look for special character that separates json strings
-            i = msg_str.find(chr(255))
+            recv_str += client.recv(1024)
             #if the special character is found
-            if i > 0:
-                #slice out the first json string from the message string
-                json_str = msg_str[:i]
-                #cut the json string from the front of the message string
-                msg_str = msg_str[i+1:]
-                #try passing the json string
-                try:
-                    json_dict = json.loads(json_str)
-                    print(json_dict["token"])
-                    print(len(json_dict["dict"]))
-                except Exception as e:
-                    print(e)
+            if len(recv_str)>0:
+                if recv_str[-1] == chr(255):
+
+                    try:
+                        recv_dict = json.loads(recv_str[:-1])
+                        send_dict = self.process_dict(recv_dict)
+                        send_str = json.dumps(send_dict)
+                        client.sendall(send_str)
+                    except Exception as e:
+                        print(e)
+                    recv_str = ""
+
 
             time.sleep(0.01)
             if time.time() - start_time > 1.0:
